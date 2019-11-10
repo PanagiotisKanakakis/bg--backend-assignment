5d0a96a8bfa746558b63b64dbf1ff43235b49ddd
pkanakakis <pkanakakis@di.uoa.gr>
2019-11-10 19:53:44 +0200

    Initial commit

---
+++ src/test/java/com/blueground/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueground.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
